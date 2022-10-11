9fa69615693b0d0ad1f9442e5a4fe33967cff7a6
sihancao <sihancao496@gmail.com>
2022-10-12 00:45:04 +0800

    构建了cbg的queen邻接矩阵

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import geopandas as gp 
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 import function
-
+# import pysal as ps
+from libpysal import weights
+import os
+import networkx as nx
+# os.environ['PROJ_LIB']=r'D:\Users\19688\Anaconda2\envs\geopands2\Lib\site-packages\osgeo\data\proj'
 #%% 读取目标county的CBG数据
 
 # cbg_df = gp.read_file(r"D:\Project\GNN_resilience\data\texas_cbg\cbg_1\Export_Output.shp") # 读取整个德克萨斯州的CBG的shapefile
@@ -25,6 +30,20 @@ import function
 cbg_df = gp.read_file(r'D:\Project\GNN_resilience\data\harris_cbg\harris.shp')
 
 # 基于cbg_df["geometry"]计算出来对应polygon的质心
+cbg_df["centroid"] = cbg_df["geometry"].map(lambda x: x.centroid)
+
+## 构建Queeen 邻接矩阵
+queen = weights.Queen.from_dataframe(cbg_df)
+# 构建queen邻接图
+queen_graph = queen.to_networkx()
+
+#  可视化
+centroids = np.array([[each.xy[0][0], each.xy[1][0]] for each in cbg_df["centroid"]])
+positions = dict(zip(queen_graph.nodes, centroids))
+ax = cbg_df.plot(linewidth=1, edgecolor="grey", facecolor="lightblue")
+ax.axis("off")
+nx.draw(queen_graph, positions, ax=ax, node_size=5, node_color="r")
+plt.show()
 
 #%% 
 
